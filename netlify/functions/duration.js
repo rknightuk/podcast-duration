e695e0cf3d88d9427025ae8613b5ffa0b626e0c8
Robb Knight <desk@rknight.me>
2023-09-29 14:14:52 +0100

    Clean up in prep for finding feeds

--- netlify/functions/duration.js
+++ netlify/functions/duration.js
@@ -15,45 +15,59 @@ const getLengthData = (length) => {
     }
 }
 
+const attemptFetchFeed = async (feed) => {
+    const response = await fetch(feed)
+    let data = await response.text()
+    try {
+        data = xml2Json.toJson(data, { object: true })
+    } catch (e)
+    {
+        return null
+    }
+
+    const episodes = (Array.isArray(data.rss.channel.item) ? data.rss.channel.item : [data.rss.channel.item])
+    const length = episodes.reduce((memo, l) => {
+        const duration = l['itunes:duration']
+        if (!duration) return memo
+        if (!duration.includes(':'))
+        {
+            memo += parseInt(duration, 10)
+            return memo
+        }
+        const parts = duration.split(':').map(d => parseInt(d, 10))
+        let seconds = 0
+        if (parts.length === 3)
+        {
+            seconds += parts[0] * 60 * 60
+            seconds += parts[1] * 60
+            seconds += parts[2]
+        } else {
+            seconds += parts[0] * 60
+            seconds += parts[1]
+        }
+        memo += parseInt(seconds, 10)
+        return memo
+    }, 0)
+
+    return {
+        title: data.rss.channel.title,
+        episodes: episodes.length,
+        link: data.rss.channel.link,
+        artwork: data.rss.channel['itunes:image'] ? data.rss.channel['itunes:image'].href : null,
+        duration: getLengthData(length),
+        average: getLengthData(Math.floor(length / episodes.length)),
+    }
+}
+
 export const handler = async (event) => {
     const feed = event.queryStringParameters.feed
-    const data = await fetch(feed)
-        .then(response => response.text())
-        .then(str => xml2Json.toJson(str, { object: true }))
-        .then(data => {
-            const episodes = (Array.isArray(data.rss.channel.item) ? data.rss.channel.item : [data.rss.channel.item])
-            const length = episodes.reduce((memo, l) => {
-                const duration = l['itunes:duration']
-                if (!duration) return memo
-                if (!duration.includes(':'))
-                {
-                    memo += parseInt(duration, 10)
-                    return memo
-                }
-                const parts = duration.split(':').map(d => parseInt(d, 10))
-                let seconds = 0
-                if (parts.length === 3)
-                {
-                    seconds += parts[0] * 60 * 60
-                    seconds += parts[1] * 60
-                    seconds += parts[2]
-                } else {
-                    seconds += parts[0] * 60
-                    seconds += parts[1]
-                }
-                memo += parseInt(seconds, 10)
-                return memo
-            }, 0)
+    
+    let data = await attemptFetchFeed(feed)
 
-            return {
-                title: data.rss.channel.title,
-                episodes: episodes.length,
-                link: data.rss.channel.link,
-                artwork: data.rss.channel['itunes:image'] ? data.rss.channel['itunes:image'].href : null,
-                duration: getLengthData(length),
-                average: getLengthData(Math.floor(length / episodes.length)),
-            }
-        })
+    if (!data)
+    {
+        // todo attempt to find feed
+    }
 
     return {
         headers: {

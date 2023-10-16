4adeb64157d860ff935cbd36fa5b37c3095f9a0d
Robb Knight <desk@rknight.me>
2023-10-16 20:41:51 +0100

    Long and short episodes

--- netlify/functions/duration.js
+++ netlify/functions/duration.js
@@ -6,7 +6,7 @@ const getLengthData = (length) => {
     const hours = Math.floor(length / 60 / 60);
     const minutes = Math.floor(length / 60) - (hours * 60);
     const seconds = length % 60;
-    const formatted = `${hours ? `${hours} hours,` : ''} ${minutes} minutes${seconds ? ` and ${seconds} seconds` : ''}`
+    const formatted = `${hours ? `${hours} hours, ` : ''}${minutes} minutes${seconds ? ` and ${seconds} seconds` : ''}`
 
     return {
         hours,
@@ -26,27 +26,46 @@ const attemptFetchFeed = async (feed) => {
         return null
     }
 
+    let longestEpisode = null
+    let shortestEpisode = null
     const episodes = (Array.isArray(data.rss.channel.item) ? data.rss.channel.item : [data.rss.channel.item])
     const length = episodes.reduce((memo, l) => {
         const duration = l['itunes:duration']
         if (!duration) return memo
+        let seconds = 0
         if (!duration.includes(':'))
         {
-            memo += parseInt(duration, 10)
-            return memo
-        }
-        const parts = duration.split(':').map(d => parseInt(d, 10))
-        let seconds = 0
-        if (parts.length === 3)
-        {
-            seconds += parts[0] * 60 * 60
-            seconds += parts[1] * 60
-            seconds += parts[2]
+            seconds = parseInt(duration, 10)
         } else {
-            seconds += parts[0] * 60
-            seconds += parts[1]
+            const parts = duration.split(':').map(d => parseInt(d, 10))
+            if (parts.length === 3)
+            {
+                seconds += parts[0] * 60 * 60
+                seconds += parts[1] * 60
+                seconds += parts[2]
+            } else {
+                seconds += parts[0] * 60
+                seconds += parts[1]
+            }
         }
-        memo += parseInt(seconds, 10)
+        const currentSeconds = parseInt(seconds, 10)
+        if (!shortestEpisode || currentSeconds < shortestEpisode.length)
+        {
+            shortestEpisode = {
+                title: l.title,
+                link: l.link,
+                length: currentSeconds,
+            }
+        }
+        if (!longestEpisode || currentSeconds > longestEpisode.length)
+        {
+            longestEpisode = {
+                title: l.title,
+                link: l.link,
+                length: currentSeconds,
+            }
+        }
+        memo += currentSeconds
         return memo
     }, 0)
 
@@ -57,6 +76,14 @@ const attemptFetchFeed = async (feed) => {
         artwork: data.rss.channel['itunes:image'] ? data.rss.channel['itunes:image'].href : null,
         duration: getLengthData(length),
         average: getLengthData(Math.floor(length / episodes.length)),
+        longestEpisode: {
+            ...longestEpisode,
+            length: getLengthData(longestEpisode.length)
+        },
+        shortestEpisode: {
+            ...shortestEpisode,
+            length: getLengthData(shortestEpisode.length)
+        },
     }
 }
 
@@ -82,7 +109,7 @@ export const handler = async (event) => {
 
     return {
         headers: {
-        'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
         },
         statusCode: 200,
         body: JSON.stringify(data)
